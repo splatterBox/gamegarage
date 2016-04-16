9d93f21b961e51f13664a1d0b297edefecfd7e13
splatterBox <timstello@yahoo.com>
2016-04-16 01:35:22 +0000

    Almost forgot the database changes!

--- garage.sql
+++ garage.sql
@@ -59,6 +59,7 @@ INSERT INTO games (title, price, discountprice) VALUES ('Space Pirates and Zombi
 INSERT INTO games (title, price, discountprice) VALUES ('Superbrothers: Sword & Sworcery EP', 7.99, 3.99);
 INSERT INTO games (title, price, discountprice) VALUES ('Thomas Was Alone', 9.99, 4.99);
 INSERT INTO games (title, price, discountprice) VALUES ('Transistor', 19.99, 11.99);
+INSERT INTO games (title, price, discountprice) VALUES ('Bastion', 14.99, 8.88); 
  
 DROP TABLE IF EXISTS gamedetails;
 CREATE TABLE gamedetails (
@@ -82,7 +83,8 @@ INSERT INTO gamedetails (gid, gdesc, votes, artpath) VALUES (12, 'Explore a rand
 INSERT INTO gamedetails (gid, gdesc, artpath) VALUES (13, 'If you''re the sort of gamer who likes unsolved mysteries and plot intepretation then you will love everything Sword & Sworcery has to offer.  The Scythian is on a woeful journey in search of the Golden Trigon. It is her purpose to tame each of the three pieces, obtain the Megatome, and put a stop to an acnient evil.  Dog is the companion of the Scythian. He accompanies the hero throughout most of the game and proves to be useful on a number of occasions. It''s unclear whether he belongs to the Scythian or Logfella.', 'css/images/Superbrothers-Sword-Sworcery-EP.jpg');
 INSERT INTO gamedetails (gid, gdesc, artpath) VALUES (14, 'In the game, the player controls one or more simple polygon shapes representing several out-of-control artificial intelligence entities, working with the shapes to get each to their individual end points on each level. Each shape is characterized with a unique name and personality, including the eponymous Thomas, which are conveyed to the player through the use of a narrator, voiced by Danny Wallace and whose performance earned the game a BAFTA Games Award.', 'css/images/Thomas_Was_Alone.jpg');
 INSERT INTO gamedetails (gid, gdesc, votes, artpath) VALUES (15, 'Cloudbank is a hand-drawn, utopian-variant of Blade Runner’s neon-soaked future, and the battleground for robotic evil-doers the Process. Standing in their way is Red: a pop star swinging a giant green sword that talks.  That sword is the Transistor, which not only acts at the excellent narrator of the game, but is also the key mechanical component of the game’s battle system.  Transistor is an absolute mental work-out. Kill things in the wrong order and you can make things magnitudes worse for yourself.  A haunting, ethereal soundtrack accompanies the game.', 7, 'css/images/Transistor.jpg');
- 
+INSERT INTO gamedetails (gid, gdesc, artpath) VALUES (16, 'Bastion is the first title from Supergiant Games, an original action role-playing game set in a lush imaginative world, in which players must create and fight for civilization''s last refuge as a mysterious narrator marks their every move.  Something called the "Great Calamity" has rolled in and taken out your entire civilization. You awaken as "the Kid," and set off to restore the Bastion -- a spot where everyone was supposed to meet if bad stuff went down. To do this, you''ll need to dive into levels, best foes, and collect shards. The graphics are amazing; the scenes are like watercolor paintings with stained glass influences.  Toss in some fantastic music and sound effects, and Bastion is a blast to experience.', 'css/images/Bastion_Boxart.jpg');
+
 DROP TABLE IF EXISTS userlibrary;
 CREATE TABLE userlibrary (
   gid int REFERENCES games(gid) NOT NULL,
